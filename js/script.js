36aa091a613dd20487dfe168d84d4c91c742dab4
HeatherEGGG46 <heather.giangregorio@gmail.com>
2025-09-21 19:20:43 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,18 @@
-function upDate(previewPic){
-   var src = previewPic.getAttribute( "src" );
-   var alt = previewPic.getAttribute( "alt" );
-   document.getElementById('image').style.backgroundImage = "url('" + src + "')";
-   document.getElementById('image').innerHTML = alt;
-     console.log('upDate event triggered!');
+function addIndex(){
+    let images= document.querySelectorAll(".preview");
+    console.log(images.length);
+    for(let i =0;i<images.length;i++){
+        console.log("image " + i);
+        images[i].setAttribute("tabindex", "0");
+    }  
 }
 
-  function unDo(previewPic){
-   previewPic = document.getElementById('image');
-   previewPic.style.backgroundImage = "url('')";
-    document.getElementById('image').innerHTML = "Hover over an image below to display here";
-      console.log('unDo event triggered!');
-  }
+function upDate(previewPic){    
+    document.getElementById("image").style.backgroundImage = `url(${previewPic.src})`;
+    document.getElementById('image').innerHTML = previewPic.alt;
+	}
+
+function unDo(){
+    document.getElementById("image").style.backgroundImage = `url(${"   "})`;
+    document.getElementById('image').innerHTML = "Hover over an image below to display here.";
+	}
